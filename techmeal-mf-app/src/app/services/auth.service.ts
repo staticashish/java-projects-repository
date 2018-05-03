8c9f4a20f096529fd955dee97034a08266cce8a1
Ashish Sonawane <ashish280386@gmail.com>
2018-05-03 22:49:16 +0530

    delete node module

--- techmeal-mf-app/src/app/services/auth.service.ts
+++ techmeal-mf-app/src/app/services/auth.service.ts
@@ -11,18 +11,22 @@ export class AuthService {
   constructor(private http: HttpClient, private router: Router) { }
 
   obtainAccessToken(loginData) {
-    const params = new HttpParams();
+    const params = new URLSearchParams();
     params.set('username', loginData.username);
     params.set('password', loginData.password);
     params.set('grant_type', 'password');
     params.set('client_id', 'client');
-    const headers = new HttpHeaders();
-    headers.set('Content-type', 'application/x-www-form-urlencoded; charset=utf-8');
-    headers.set('Authorization', 'Basic ' + btoa('client:client'));
 
-   const tokenUrl = 'http://localhost:8005/gateway/auth/oauth/token';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/x-www-form-urlencoded; charset=utf-8',
+        'Authorization': 'Basic ' + btoa('client:client')
+    })
+    };
+
+  const tokenUrl = 'http://techmeal-mf-gateway-service.herokuapp.com/gateway/auth/oauth/token';
     // const tokenUrl = 'http://techmeal-mf-auth-server.herokuapp.com/auth/oauth/token';
-     this.http.post(tokenUrl, params.toString(), { headers : headers } )
+     this.http.post(tokenUrl, params.toString(), httpOptions )
       .subscribe(
         data => console.log(data),
         err => alert('Invalid Credentials' + JSON.stringify(err)));
