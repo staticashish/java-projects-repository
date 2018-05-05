642a48174c621f40aef10ecc577e556d559bbce9
Ashish Sonawane <ashish280386@gmail.com>
2018-05-06 00:58:03 +0530

    revert ResourceServerConfig

--- techmeal-mf-auth-server/src/main/java/io/techmeal/config/ResourceServerConfig.java
+++ techmeal-mf-auth-server/src/main/java/io/techmeal/config/ResourceServerConfig.java
@@ -2,6 +2,8 @@ package io.techmeal.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -17,5 +19,15 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenServices(defaultTokenServices);
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+			.and()
+				.authorizeRequests()
+				.anyRequest()
+				.permitAll();
 	}
 }
