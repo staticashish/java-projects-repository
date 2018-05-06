3c455eb6574c9c229e5208d090f185ac75304307
Ashish Sonawane <ashish280386@gmail.com>
2018-05-06 23:52:50 +0530

    Added @ResponseBody annotation for createrole

--- techmeal-mf-db-service/src/main/java/io/techmeal/rest/V1/RoleController.java
+++ techmeal-mf-db-service/src/main/java/io/techmeal/rest/V1/RoleController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,7 @@ public class RoleController {
 	}
 	
 	@PostMapping("/create")
-	public ResponseEntity<RoleDto> create(RoleDto roleDto) {
+	public ResponseEntity<RoleDto> create(@RequestBody RoleDto roleDto) {
 		RoleDto newRoleDto = roleService.createRole(roleDto);
 		
 		return new ResponseEntity<>(newRoleDto, HttpStatus.OK);
